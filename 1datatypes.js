53357e7de9aca704fcba424c03c498b218e14208
aakashdeveloper <ahanda205@gmail.com>
2021-02-04 08:52:04 +0530

    a

---
+++ 1datatypes.js
@@ -0,0 +1,115 @@
+String  = "Hi" 'bie' "434" '434.4' 'true' "false"
+Number = 8978 89787878 8.78 .877 898989.878877
+Boolean = true/false
+null
+undefined
+
+//es5////
+Declare Variable
+
+var a = 10
+var b = 'Hiii'
+var c = true
+
+typeof(a)
+"number"
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+var d  = "10"
+
+typeof(d)
+"string"
+var e = 10.44
+
+typeof(e)
+"number"
+
+////////////////////////
+var a = 10
+var b = 20
+
+a+b
+30  (addition)
+a-b
+-10
+a*b
+200
+a/b
+0.5
+
+
+var a =4
+var b =2
+
+a%b
+0
+4%3
+1
+4%1
+0
+5%3
+2
+
+
+2%4
+2
+3%4
+3
+4%4
+0
+
+var a = "Hi"
+var b = "JavaScript"
+
+a+b
+"HiJavaScript" (concat)
+a-b
+NaN (not a number)
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+
+
+var c = 10
+undefined
+a+b
+"HiJavaScript"
+a+c
+"Hi10"
+a-c
+NaN
+c+a
+"10Hi"
+a + ' '+b
+"Hi JavaScript"
+
+String+String=String
+String+Number= String
+Number+String=String
+Number+Number=Number
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+"10"+20+30
+"1020"+30
+"102030"
+
+10+"20"+30-1
+102029
+
+10+20+"30"-1
+3029
+
+"10"+20+30-1
+102029
