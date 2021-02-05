506e76757fc45dc4dbbef8bddfdd0568c631cb41
aakashdeveloper <ahanda205@gmail.com>
2021-02-05 08:40:36 +0530

    A

---
+++ 2datatypes.js
@@ -0,0 +1,121 @@
+var a = "10"
+var b = "1"
+
+a+b
+"101"
+
+var a = "10"
+var b = "1"
+
+a+b
+"101"
+"101"
+//es5
+parseInt(a)+parseInt(b)
+11
+parseInt(a)
+10
+var c ="10.21"
+undefined
+parseInt(c)
+10
+parseFloat(c)
+10.21
+parseFloat(a)
+10
+
+//es6
+var a = "10"
+undefined
+Number(a)
+10
+var a = "10.333"
+undefined
+Number(a)
+10.333
+
+var a ="123delhi"
+var b ="delhi123"
+
+
+var c ="hi"
+undefined
+parseInt(c)
+NaN
+Number(c)
+NaN
+
+var a ="123delhi"
+var b ="delhi123"
+
+parseInt(a)
+parseInt(b)
+
+
+
+Number(a)
+
+var a ="123delhi"
+var b ="delhi123"
+undefined
+parseInt(a)
+123
+parseInt(b)
+NaN
+var a = "1a1b"
+undefined
+Number(a)
+NaN
+parseInt(a)
+1
+
+
+
+////////
+var a= true
+var b = false
+
+true is equal to 1
+false is equal to 0
+
+var a= true
+var b = false
+undefined
+a+b
+1
+a-b
+1
+10-true
+9
+10*false
+0
+true/false
+Infinity
+var a = "hi"
+undefined
+a=true
+true
+var a = "hi"
+undefined
+a+true
+"hitrue"
+var a ="true"
+undefined
+var b="true"
+undefined
+a+b
+"truetrue"
+a-b
+NaN
+
+var a = 10
+isNaN(a)
+false
+
+var b = "10"
+isNaN(b)
+false
+
+var c ="Hi"
+isNaN(c)
+true
